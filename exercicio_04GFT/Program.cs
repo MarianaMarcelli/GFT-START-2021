46e398fd6f6d63355abf0a50e6e63c2303783362
Mariana Marcelli <marimmarcelli@gmail.com>
2021-02-08 10:52:12 -0300

    Exercício 4

---
+++ exercicio_04GFT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicio_04GFT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com a palavra 1: ");
+            var A = Console.ReadLine();
+            Console.Write("Entre com a palavra 2: ");
+            var B = Console.ReadLine();
+
+            var transitoria = A;
+            A = B;
+            B = transitoria;
+
+            Console.WriteLine($"{A} {B}");
+        }
+    }
+}
