b34264a26146e59d5790e79862a7f36f0d1e2fe3
Mariana Marcelli <marimmarcelli@gmail.com>
2021-02-08 10:43:23 -0300

    Exercicio3

---
+++ exercicio_03GFT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicio_03GFT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            var numeroDigitado = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numeroDigitado; i++)
+            {
+                Console.Write("Incomoda ");
+            }
+
+            Console.Write("muito mais");
+        }
+    }
+}
+
